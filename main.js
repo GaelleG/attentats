719c4434ee40bee06a9ff95ba44b81e65d738c02
Gaelle Gomez <pussy@pukogames.com>
2016-06-30 00:28:48 +0200

    Flex display

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var moment = require('moment');
 
 var attacks = require('./src/attentats.json');
 var chronos = document.querySelector('#chronos');
@@ -10,9 +11,13 @@ _(attacks)
 function addAttackEl(attack) {
   var el = document.createElement('div');
   el.classList.add('attack');
-  el.innerHTML = '<div class="attack__date">' + attack.date + '</div>' +
+  el.innerHTML = '<div class="attack__label">' +
+    '<div class="attack__date">' + moment(attack.date).format('Y/MM/DD') + '</div>' +
     '<div class="attack__country">' + attack.country + '</div>' +
+    '</div>' +
+    '<div class="attack__numbers">' +
     '<div class="attack__deaths">' + attack.deaths + '</div>' +
-    '<div class="attack__injuries">' + attack.injuries + '</div>';
+    '<div class="attack__injuries">' + attack.injuries + '</div>' +
+    '</div>';
   chronos.appendChild(el);
 }
